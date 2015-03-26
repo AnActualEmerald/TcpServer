2ab7c77713d3e730b9fc6132bbd8ef4144b26611
Burrito119 <kevinliger13@gmail.com>
2015-03-26 14:32:58 -0400

    Initial Commit

---
+++ Server.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kgauthier16
+ * Date: 3/24/2015
+ * Time: 8:18 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPServer
+{
+	class Server
+	{
+		TcpListener _server;
+		List<Socket> clients = new List<Socket>();
+		byte[] buff = new byte[1024];
+		
+		
+		public Server(IPAddress addr, int port)
+		{
+			_server = new TcpListener(addr, port);
+		}		
+		
+		public Server Start()
+		{
+			_server.BeginAcceptSocket(new AsyncCallback(AcceptCall), null);
+			return this;
+		}
+		
+		#region Async callbacks
+		private void AcceptCall(IAsyncResult r)
+		{
+			Socket c = _server.EndAcceptSocket(r);
+			clients.Add(c);
+			c.BeginReceive(buff, 0, buff.Length, SocketFlags.None, new AsyncCallback(ReceiveCall), null);
+			_server.BeginAcceptSocket(new AsyncCallback(AcceptCall), null);
+		}
+		
+		private void ReceiveCall(IAsyncResult r)
+		{
+			Socket c = r.AsyncState as Socket;
+			byte[] rec = new byte[c.EndReceive(r)];
+			Array.Copy(buff, rec, rec.Length);
+			
+			Console.WriteLine("Client sent: " + Encoding.ASCII.GetString(rec));
+		}
+		
+		private void SendCall(IAsyncResult r)
+		{
+			Socket c = r.AsyncState as Socket;
+			c.EndSend(r);
+		}
+		#endregion
+		
+		public void PerformCommand(String command, String[] args)
+		{
+			if(command.ToLower() == "send")
+			{
+				bool all = false;
+				String message = "";
+				foreach(String s in args)
+				{
+					String ss = s.Trim();
+					if(ss.Contains("-all"))
+						all = true;
+					
+					if(!ss.StartsWith("-"))
+						message = ss;
+				}
+				
+				Console.WriteLine("Will send \"" + message + "\" to clients. All was " + all);
+			}
+			
+			if(command.ToLower() == "ping")
+				Console.WriteLine("pong");
+		}
+		
+		private void SendText(String text, Socket[] targets)
+		{
+			byte[] str = Encoding.ASCII.GetBytes(text);
+			foreach(Socket c in targets)
+				c.BeginSend(str, 0, str.Length, SocketFlags.None, new AsyncCallback(SendCall), null);
+		}
+	}
+}
---
+++ Starter.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kgauthier16
+ * Date: 3/24/2015
+ * Time: 8:43 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+namespace TCPServer
+{
+	class Starter
+	{
+		static Server s;
+		public static void Main(string[] args)
+		{
+			IPAddress addr;
+			Console.WriteLine("enter ip: ");
+			string ip = Console.ReadLine();
+			if(ip == "any")
+				addr = IPAddress.Any;
+			else
+				addr = IPAddress.Parse(ip);
+			
+			Console.WriteLine("enter port: ");
+			int port = int.Parse(Console.ReadLine());
+			s = new Server(addr, port);
+			
+			Run();
+		}
+		
+		public static void Run()
+		{
+			
+			while(true)
+			{
+				Console.WriteLine("Enter Command: ");
+				Command c = parseCommand(Console.ReadLine() + " ");
+				s.PerformCommand(c.command, c.args);
+			}
+		}
+		
+		public static Command parseCommand(string c_string)
+		{
+			char[] chars = c_string.ToCharArray();
+			List<char> part = new List<char>();
+			List<string> fin_parts = new List<string>();
+			bool isMessage = false;
+			for(int i = 0; i < chars.Length; i++)
+			{
+				if(chars[i] == '\"'){
+					isMessage = !isMessage;
+					continue;
+				}
+				
+				if(chars[i] == ' ' && !isMessage){
+					fin_parts.Add(new string(part.ToArray()));
+					part.Clear();
+				}
+				
+				part.Add(chars[i]);
+			}
+			
+			string command = fin_parts[0];
+			fin_parts.Remove(command);
+			
+			return new Command()
+			{
+				command = command,
+				args = fin_parts.ToArray()
+			};
+		}
+	}
+	
+	struct Command
+	{
+		public string command;
+		public string[] args;
+	}
+}
